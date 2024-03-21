313c3452a6c659d0136f7124a87603cfce3b48dc
doku <89829952+dokusaido@users.noreply.github.com>
2024-03-21 18:06:04 +1100

    Changed some minor things

--- server/main.lua
+++ server/main.lua
@@ -51,7 +51,7 @@ RegisterNetEvent("e_groups:server:CreateGroup", function()
 	local src = source
 	local isOwner = isPlayerGroupOwner(src)
 	local isInGroup = isPlayerInGroup(src)
-	if isOwner or isOwner and isInGroup or isInGroup then
+	if isOwner or isInGroup then
 		Notify(src, "You already have a group!", "inform")
 		return
 	end
@@ -140,7 +140,7 @@ RegisterNetEvent("e_groups:server:AddMember", function(targetId)
 		Player(targetId).state.groups = { ownerid = src }
 		return
 	else
-		Notify(src, ("Player with id: %s refused your invite!"):format(targetId), "inform"
+		Notify(src, ("Player with id: %s refused your invite!"):format(targetId), "inform")
 	end
 end)
 
